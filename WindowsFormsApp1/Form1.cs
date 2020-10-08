e1a0e939cb3d68baa662858ca05fca74994ecea2
aaromachev <72490204+aaromachev@users.noreply.github.com>
2020-10-08 18:48:33 +0300

    VSE PEREDELAL, KOROCHE ETO BAZA

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,63 +19,10 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            
-            //Файл/Сохранить как
-        }
-
-        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //Файл/Сохранить
-
-        }
-        private void toolStripMenuItem1_Click(object sender, EventArgs e)
-        {
-            //Правка/Копировать
-        }
-
-        private void toolStripMenuItem2_Click(object sender, EventArgs e)
-        {
-            //Правка/Вставить
-        }
-
-        private void toolStripMenuItem3_Click(object sender, EventArgs e)
-        {
-            //Правка/Вырезать
-        }
-
-        private void toolStripMenuItem4_Click_1(object sender, EventArgs e)
-        {
-            //Вид/Шрифт
-        }
-
-        private void настройкиToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //Настройки
-            //this.Close();
-            TH = new Thread(open);
-            TH.SetApartmentState(ApartmentState.STA);
-            TH.Start();
-        }
-        private void toolStripComboBox1_Click(object sender, EventArgs e)
-        {
-            //Прогноз/Создать
-        }
-
-        private void toolStripMenuItem4_Click(object sender, EventArgs e)
-        {
-            //Прогноз/История
-        }
-
-        private void toolStripComboBox1_Click_1(object sender, EventArgs e)
-        {
-            //Прогноз/Поделиться
-        }
-
         public void open(object obj)
         {
             Application.Run(new Settings());    
         }
+
     }
 }
